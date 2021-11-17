a9c6d93379545221eba2a9efa4d9b907ebbf9a2f
valchez-dev <valchez.dev@yahoo.com>
2021-11-17 21:58:23 +0300

    bottom tab bar navigation (tasks/completed)

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import welcome from './src/screens/welcome';
 import home from './src/screens/home';
+import done from './src/screens/done';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen options={{headerShown: false}} name="ToDo" component={home} />
+      <Tab.Screen options={{headerShown: false}} name="Done" component={done} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
@@ -15,9 +27,8 @@ const App = () => {
           component={welcome}
           options={{headerShown: false}}
         />
-        <Stack.Screen 
-        name="Home" 
-        component={home} />
+        <Stack.Screen name="Home" component={HomeTabs} />
+        <Stack.Screen name="Done" component={done} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/done.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+const done = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Done Tasks Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  text: {},
+});
+
+export default done;
