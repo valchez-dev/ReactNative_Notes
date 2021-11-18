eb6593eb773abb1f70b09200a5914bfeed3fb44f
valchez-dev <valchez.dev@yahoo.com>
2021-11-18 10:42:22 +0300

    Home screen getTasks redux-function

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect } from 'react';
 import {Text, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {setTasks, setTaskID} from '../redux/actions';
 
 const Home = ({navigation}) => {
+  const {tasks} = useSelector(state => state.taskReducer);
+  const dispatch = useDispatch();
+
+
+  useEffect(()=>{
+    getTasks()
+  },[])
+
+  const getTasks = () => {
+    AsyncStorage.getItem('tasks')
+      .then(tasks => {
+        const parsedTasks = JSON.parse(tasks);
+        if (parsedTasks && typeof parsedTasks === 'object') {
+          dispatch(setTasks(parsedTasks));
+        }
+      })
+      .catch(e => console.log(e));
+  };
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
-      <TouchableOpacity 
-      onPress={()=> navigation.navigate("Details")} 
-      style={styles.btn}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(setTaskID(new Date().getMilliseconds()))
+          navigation.navigate('Details')}}
+        style={styles.btn}>
         <Text style={styles.btnText}>+</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -21,22 +45,22 @@ const styles = StyleSheet.create({
   },
   btn: {
     width: 60,
-    height:60,
-    borderRadius:50,
-    backgroundColor:"#FBF46D",
-    justifyContent:'center',
-    alignItems:'center',
-    position:'absolute',
-    bottom:30,
-    right:30,
-    elevation:5
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: '#FBF46D',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    elevation: 5,
   },
-  btnText:{
-    textAlign:'center',
-    fontWeight:'bold',
-    color:'#000000',
-    fontSize:30
-  }
+  btnText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#000000',
+    fontSize: 30,
+  },
 });
 
 export default Home;
