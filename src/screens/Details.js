9089ba9e728269062c3bd7f01d2d01a398fd2161
valchez-dev <valchez.dev@yahoo.com>
2021-11-19 22:08:24 +0300

    lil edits

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -8,29 +8,31 @@ import {
   Button,
   Keyboard,
   Alert,
+  TouchableOpacity,
+  Modal,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {setTasks, setTaskID} from '../redux/actions';
 
 const Details = ({navigation}) => {
   const [title, setTitle] = useState('');
+  const [modal, setModal] = useState(false);
   const [description, setDescription] = useState('');
 
   const {tasks, taskID} = useSelector(state => state.taskReducer);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    getTask()
-  },[]);
+  useEffect(() => {
+    getTask();
+  }, []);
 
   const getTask = () => {
-    const Task = tasks.find(task=> task.id === taskID);
-    if(Task){
+    const Task = tasks.find(task => task.id === taskID);
+    if (Task) {
       setTitle(Task.title);
       setDescription(Task.description);
     }
-  }
-
+  };
 
   const saveTaskButton = () => {
     Keyboard.dismiss();
@@ -43,23 +45,20 @@ const Details = ({navigation}) => {
           id: taskID,
           title,
           description,
-          checked: false
+          checked: false,
         };
-
 
         //if exist = update, if not = create
         // -1 => no element
         const index = tasks.findIndex(task => task.id === taskID);
         let newTasks = [];
 
-        if(index > -1){
+        if (index > -1) {
           newTasks = [...tasks];
-          newTasks[index]=Task;
-        }else{
-          newTasks=[...tasks, Task];
+          newTasks[index] = Task;
+        } else {
+          newTasks = [...tasks, Task];
         }
-
-
 
         AsyncStorage.setItem('tasks', JSON.stringify(newTasks))
           .then(() => {
@@ -72,12 +71,20 @@ const Details = ({navigation}) => {
         console.log(e);
       }
     }
-
-    
   };
+
+  const alarmHandler = () => {};
 
   return (
     <View style={styles.container}>
+      <Modal
+      visible={modal}
+      animationType='slide'
+      hardwareAccelerated={true}
+      onRequestClose={()=>setModal(!modal)}>
+  
+      </Modal>
+
       <TextInput
         value={title}
         onChangeText={text => setTitle(text)}
@@ -93,7 +100,13 @@ const Details = ({navigation}) => {
         placeholder="Description..."
       />
 
-      <Button onPress={saveTaskButton} title="Save" color="#0480BA" />
+      <TouchableOpacity onPress={alarmHandler} style={styles.btnAlarm}>
+        <Text style={styles.btnText}>Set Alarm</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={saveTaskButton} style={styles.btnSave}>
+        <Text style={styles.btnText}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -101,7 +114,7 @@ const Details = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#C0D2DB',
+    backgroundColor: '#94DAFF',
     padding: 10,
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -115,6 +128,32 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#000000',
+  },
+  btnAlarm: {
+    backgroundColor: '#94B3FD',
+    padding: 10,
+    margin: 10,
+    fontSize: 20,
+    width: '40%',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#000000',
+  },
+  btnSave: {
+    backgroundColor: '#94B3FD',
+    padding: 10,
+    margin: 10,
+    fontSize: 20,
+    width: '40%',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#000000',
+  },
+  btnText: {
+    textAlign: 'center',
+    color: '#0000000',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
 
